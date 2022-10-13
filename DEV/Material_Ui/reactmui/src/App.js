3b3c308f98d81d2b46906e6538f84f3bf393e421
abhishek-dev <enquireabhishek@gmail.com>
2022-10-13 23:25:44 +0530

    learnt about material icons

--- DEV/Material_Ui/reactmui/src/App.js
+++ DEV/Material_Ui/reactmui/src/App.js
@@ -1,8 +1,7 @@
 import './App.css';
 import React, { useState } from 'react';
 import Button from '@mui/material/Button';
-// import Delete from '@mui/material/Delete';
-import { Delete } from '@mui/material/icons/Delete';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 
 function App() {
@@ -20,9 +19,9 @@ function App() {
     <div className="App">
       <h1>React MUI</h1>
       <Button
-        startIcon={<Delete />}
-        variant="outlined"
-        color={color}
+        startIcon={<SettingsIcon />}
+        variant="contained"
+        color={"success"}
         onClick={() => { CustomMe() }}
         disabled={disableBtn}
         size="small" >
