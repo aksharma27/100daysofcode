f7695854187f7e5df2f303d9af24168e159f192f
abhishek-dev <enquireabhishek@gmail.com>
2022-10-22 00:34:23 +0530

    added images

--- DEV/Material_Ui/reactmui/src/components/Rightbar.jsx
+++ DEV/Material_Ui/reactmui/src/components/Rightbar.jsx
@@ -12,12 +12,13 @@ const Rightbar = () => {
             <Box position="fixed">
                 <Typography varient="h6" fontWeight={300}>Online Friends</Typography>
                 <AvatarGroup max={7} total={24}>
-                    <Avatar alt="Sagnik Bairagya" url="https://instagram.fccu2-2.fna.fbcdn.net/v/t51.2885-19/310496409_159535520087587_2721178941537380459_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.fccu2-2.fna.fbcdn.net&_nc_cat=107&_nc_ohc=VC58_cbFyWEAX_FrPIo&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AT-YiSHVHcEBgsKeKQOIaRXBppMqm3ddSmNWSzDk0aY0Gw&oe=635883E8&_nc_sid=8fd12b" />
-                    <Avatar alt="Abhishek Sharma" url="https://instagram.fccu2-2.fna.fbcdn.net/v/t51.2885-19/307979241_833306874708664_7062520864387190585_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.fccu2-2.fna.fbcdn.net&_nc_cat=104&_nc_ohc=DOlYw1IUSFMAX9ip3Ot&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AT_P60Z3VgVX95A1tgX-oy-4NFPNxRLRh0sB2zvESGYAKQ&oe=6357613D&_nc_sid=8fd12b" />
-                    <Avatar alt="Rahul Shakya" src="/static/images/avatar/4.jpg" />
-                    <Avatar alt="Renu Sharma" src="/static/images/avatar/5.jpg" />
-                    <Avatar alt="Ajay Kumar Sharma" src="/static/images/avatar/5.jpg" />
-                    <Avatar alt="Arti Shaw" src="/static/images/avatar/5.jpg" />
+                    <Avatar alt="Remy Sharp" src="https://material-ui.com/static/images/avatar/1.jpg" />
+                    <Avatar alt="Travis Howard" src="https://material-ui.com/static/images/avatar/2.jpg" />
+                    <Avatar alt="Cindy Baker" src="https://material-ui.com/static/images/avatar/3.jpg" />
+                    <Avatar alt="Remy Sharp" src="" />
+                    <Avatar alt="Travis Howard" src="https://material-ui.com/static/images/avatar/2.jpg" />
+                    <Avatar alt="Cindy Baker" src="https://material-ui.com/static/images/avatar/3.jpg" />
+                    <Avatar alt="Cindy Baker" src="https://material-ui.com/static/images/avatar/3.jpg" />
                 </AvatarGroup>
             </Box>
         </Box>
--- DEV/Material_Ui/reactmui/src/components/post.jsx
+++ DEV/Material_Ui/reactmui/src/components/post.jsx
@@ -25,7 +25,7 @@ const Post = () => {
             <CardHeader
                 avatar={
                     <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                        R
+                        AS
                     </Avatar>
                 }
                 action={
@@ -33,20 +33,22 @@ const Post = () => {
                         <MoreVertIcon />
                     </IconButton>
                 }
-                title="Abhishek Kumar Sharma"
+                title="Abhishek Sharma"
                 subheader={date.getDate() + ' - ' + date.getMonth() + ' - ' + date.getFullYear()}
             />
             <CardMedia
                 component="img"
                 height="10%"
                 image="https://images.unsplash.com/photo-1607705703571-c5a8695f18f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-                alt="Paella dish"
+                alt="Abhishek Kumar Sharma"
             />
             <CardContent>
                 <Typography variant="body2" color="text.secondary">
-                    This impressive paella is a perfect party dish and a fun meal to cook
-                    together with your guests. Add 1 cup of frozen peas along with the mussels,
-                    if you like.
+                    Abhishek is a passionate programmer, who's currently working on his projects on MERN Stack
+                    and also in hi DSA skills. He's really hard working and and always does something out of the box.
+                    This is his project on material ui, it's a prototype of a social network with feeds and sidebars showing
+                    online friends and some basic information, and navigation bar with a search componenet in it.
+                    This project is purely based on react and its components, props and material ui etc.
                 </Typography>
             </CardContent>
             <CardActions disableSpacing>
