fc8c38380bdaa9b146d3e6a5bd25d03eba73feca
abhishek-dev <enquireabhishek@gmail.com>
2022-10-26 19:44:06 +0530

    react components, React props

---
+++ DEV/myapp/src/App.js
@@ -0,0 +1,37 @@
+// import logo from './logo.svg';
+import './App.css';
+import React from 'react';  
+
+const Person = (props) =>{
+  return(
+    <>
+    <h1>Name : {props.name}</h1>
+    <h2>Last Name : {props.lastname}</h2>
+    <h2>Age : {props.age}</h2>
+    </>
+  )
+}
+
+function App() {
+  return(
+    <>
+    <Person 
+    name="Abhishek"
+     lastname="Sharma" 
+     age={20}
+     />
+
+     <Person
+      name='Sofia'
+      lastname='Iram'
+      age={21}
+      />
+    
+    
+        </>    
+  )
+
+  
+}
+
+export default App;
