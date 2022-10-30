ec876d1e3e53cf7a1fb3c23336fdff7cabfbf672
abhishek-dev <enquireabhishek@gmail.com>
2022-10-30 22:16:25 +0530

    backtracking + tailwind css

---
+++ DEV/Material_Ui/muiproj2/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Typography} from '@mui/material'
+
+function App() {
+  return (
+   <>
+   <Typography varient="h1"></Typography>
+   </>
+  );
+}
+
+export default App;
