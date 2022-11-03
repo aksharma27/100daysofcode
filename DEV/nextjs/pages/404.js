4a3f6d2ebc8f2211a11122e1d60301b8db72221a
abhishek-dev <enquireabhishek@gmail.com>
2022-11-03 23:45:26 +0530

    how to redirect to home automatically from 404 page

--- DEV/nextjs/pages/404.js
+++ DEV/nextjs/pages/404.js
@@ -1,11 +1,28 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
+import { useEffect } from "react";
 
 const ErrorPage = () => {
+  const router = useRouter();
+  const handleError = () =>{
+    router.push("./");
+  }
+
+  useEffect (()=>{
+    setTimeout(() =>{
+      router.push("./");
+    }, 3000);
+   }, []);
+
+
   return (
     <div>
       <h1>404</h1>
       <h2>We are sorry, Page not found</h2>
       <Link href="./">go to home</Link>
+      <button>
+        <a onClick={handleError} >Go back to home</a>
+      </button>
     </div>
   )
 }
