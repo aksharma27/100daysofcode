9cd7d94ba580cc5a3a333e8395bbcb9c8b5bff2f
abhishek-dev <enquireabhishek@gmail.com>
2022-11-04 19:36:50 +0530

    component based module.css + import styles for component based css

---
+++ DEV/nextjs/pages/contact.js
@@ -0,0 +1,12 @@
+// import React from 'react'
+import styles from "../styles/contact.module.css";
+
+const contact = () => {
+  return (
+    <div>
+      <h1 className={styles.heading1}>Hello to contact page</h1>
+    </div>
+  )
+}
+
+export default contact
