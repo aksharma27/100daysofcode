9f9d8039d9daae4b008dd5ca03c8ca12ed9ed922
abhishek-dev <enquireabhishek@gmail.com>
2022-11-05 01:30:13 +0530

    linkedlist add first, last and at

--- DSA/LinearSearch/src/LinkedList/LinkedList.java
+++ DSA/LinearSearch/src/LinkedList/LinkedList.java
@@ -44,7 +44,11 @@ public class LinkedList {
     static int getSize(){
         Node temp = head;
         int count = 0;
+        if (temp == null){
+            return Integer.MIN_VALUE;
+        }
         while (temp != null){
+            temp = temp.next;
             count++;
         }
         return count;
@@ -83,15 +87,62 @@ public class LinkedList {
         return temp.data;
     }
 
+
+
+    //add first
+    static void addFirst(int data){
+        Node first = new Node(data);
+        Node temp = head;
+        if (temp == null){
+            head = first;
+        }
+        else {
+            first.next = head;
+            head = first;
+        }
+    }
+
+
+    //add at:
+    static void addAt(int data, int prevIndex){
+        Node at = new Node(data);
+        int count = 0;
+        Node temp = head;
+        while (temp != null && count != prevIndex){
+            temp = temp.next;
+            count++;
+        }
+        Node tempNext = temp.next;
+        temp.next = at;
+        at.next = tempNext;
+    }
+
+
+
+    //add last:
+    static void addLast(int data) {
+        Node lastNew = new Node(data);
+        Node temp = head;
+        while (temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = lastNew;
+    }
+
     public static void main(String[] args) {
         createList(2);
         createList(3);
         createList(4);
         createList(0); createList(19); createList(40);
-        display();
+//        display();
 //        System.out.println("\n" + getAt(3));
-        System.out.println(getFirst() + " " + getLast());
-
+//        System.out.println(getFirst() + " " + getLast());
+          addFirst(90);
+          addAt(100, 2);
+          addLast(11);
+          display();
+          int size = getSize();
+        System.out.println(size);
     }
 }
 
