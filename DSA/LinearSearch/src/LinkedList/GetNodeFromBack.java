d504f514dff0693a1c395043f19ff31d36f2b53f
abhishek-dev <enquireabhishek@gmail.com>
2022-11-06 23:37:54 +0530

    find Kth element from end of linkedlist :

--- DSA/LinearSearch/src/LinkedList/GetNodeFromBack.java
+++ DSA/LinearSearch/src/LinkedList/GetNodeFromBack.java
@@ -9,8 +9,37 @@ public class GetNodeFromBack {
             this.data = data;
         }
     }
+
+    static void addNode(int data){
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
     static Node head;
     static int getFromEnd(int index){
-        
+        Node slow = head;
+        Node fast = head;
+        for (int i = 0; i < index; i++){
+            fast = fast.next;
+        }
+
+        while (fast.next != null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow.data;
+    }
+
+    public static void main(String[] args) {
+       addNode(1);
+       addNode(2);
+       addNode(3);
+       addNode(4);
+       addNode(5);
+       addNode(6);
+
+        int ans = getFromEnd(4);
+        System.out.println(ans);
     }
 }
