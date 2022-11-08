d5b1494eef3a868f6cffc81f4fd6dcc4702eae8d
abhishek-dev <enquireabhishek@gmail.com>
2022-11-08 20:35:26 +0530

    audio in js

--- DEV/JS/event.js
+++ DEV/JS/event.js
@@ -2,6 +2,7 @@ var noButtons = document.querySelectorAll("button").length;
 
 for (var i = 0; i < noButtons; i++) {
     document.querySelectorAll("button")[i].addEventListener("click", function(){
-        alert("all buttons got clicked");
+        var audio = new Audio("");
+        audio.play();
     })
 }
