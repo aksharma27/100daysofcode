565d6d7f94d2a8e4ad9ec7d84c959637100290b4
abhishek-dev <enquireabhishek@gmail.com>
2022-11-08 20:53:54 +0530

    js objects

--- DEV/JS/event.js
+++ DEV/JS/event.js
@@ -4,5 +4,8 @@ for (var i = 0; i < noButtons; i++) {
     document.querySelectorAll("button")[i].addEventListener("click", function(){
         var audio = new Audio("");
         audio.play();
+            alert("Hi " + housekeeper1.name + " is " + housekeeper1.age + " years old"
+            + " and her hobbies are: " + housekeeper1.hobbies + "Hi " + housekeeper2.name + " is " + housekeeper2.age + " years old"
+            + " and her hobbies are: " + housekeeper2.hobbies);    
     })
 }
---
+++ DEV/JS/objects.js
@@ -0,0 +1,12 @@
+var housekeeper1 = {
+    name: "Angela",
+    age: 30,
+    hobbies: ["coding", "sweeping", "gaming"],
+    married: true
+}
+var housekeeper2 = {
+    name: "Ankita",
+    age: 30,
+    hobbies: ["messing", "sleeping", "chilling"],
+    married: false
+}
