34ec888f4e8aae4d77813a3287b2ef5e4fcbfd2c
abhishek-dev <enquireabhishek@gmail.com>
2022-11-13 20:08:19 +0530

    chaining

--- DEV/JQuery/fadeTogge.js
+++ DEV/JQuery/fadeTogge.js
@@ -3,7 +3,13 @@ $("input").on("click", ()=>{
 })
 
 //animate:
+// $("button").on("click", ()=>{
+//     $("h1").animate({opacity: "0.5"},2000);
+//     $("h1").animate({margin: "20%"});
+// })
+
+
+//chaininig
 $("button").on("click", ()=>{
-    $("h1").animate({opacity: "0.5"},2000);
-    $("h1").animate({margin: "20%"});
+    $("h1").slideUp().slideDown().animate({opacity: "0.7"}, 900);
 })
