9a6d282b01603b27269e668da20eccc2f970831f
abhishek-dev <enquireabhishek@gmail.com>
2022-11-14 20:57:23 +0530

    pass data frmo ujs server

--- DEV/ejs/todolist/app.js
+++ DEV/ejs/todolist/app.js
@@ -7,33 +7,16 @@ app.set("view engine", "ejs");
 app.get("/", (req, res)=>{
    
     var today = new Date();
-    var  day = "";
-    if(today.getDay() === 0){
-        day = "Sunday";
-    }
-    else if(today.getDay() === 1){
-        day = "Monday";
-    }
-   
-    else if(today.getDay() === 2){
-        day = "Tudesday";
-    }
-   
-    else if(today.getDay() === 3){
-        day = "Wednesday";
-    }
-   
-    else if(today.getDay() === 4){
-        day = "Thursday";
-    }
-   
-    else if(today.getDay() === 5){
-        day = "Friday";
-    }
-    else{
-        day = "Saturday";
-     }
-     res.render("list", {kindOfDay: day});
+    var options = {
+        day: "numeric",
+        month: "long",
+        weekday: "long",
+        
+    };
+    var day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", {kindOfDay: day});
+     
 });
 
 app.listen(3000, ()=>{
