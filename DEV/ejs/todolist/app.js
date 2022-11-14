87f5aa1c716b12717094812961e4124b66596ed7
abhishek-dev <enquireabhishek@gmail.com>
2022-11-14 12:35:19 +0530

    ejs template engine

--- DEV/ejs/todolist/app.js
+++ DEV/ejs/todolist/app.js
@@ -1,12 +1,40 @@
-const express = require("express");
+var express = require("express");
 const bodyParser = require("body-parser");
+let ejs = require('ejs')
 
 const app = express();
-
+app.set("view engine", "ejs");
 app.get("/", (req, res)=>{
-    res.send("Hello world!");
+   
+    var today = new Date();
+    var  day = "";
+    if(today.getDay() === 0){
+        day = "Sunday";
+    }
+    else if(today.getDay() === 1){
+        day = "Monday";
+    }
+   
+    else if(today.getDay() === 2){
+        day = "Tudesday";
+    }
+   
+    else if(today.getDay() === 3){
+        day = "Wednesday";
+    }
+   
+    else if(today.getDay() === 4){
+        day = "Thursday";
+    }
+   
+    else if(today.getDay() === 5){
+        day = "Friday";
+    }
+    else{
+        day = "Saturday";
+     }
+     res.render("list", {kindOfDay: day});
 });
-
 
 app.listen(3000, ()=>{
     console.log("Server started at posrt 3000 ")
