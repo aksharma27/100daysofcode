0e87f79896059c1c04cbceee14df775144a408e6
abhishek-dev <enquireabhishek@gmail.com>
2022-11-17 20:32:55 +0530

    templating in ejs

--- DEV/ejs/todolist/app.js
+++ DEV/ejs/todolist/app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 let ejs = require('ejs')
 
+
 const app = express();
 app.set("view engine", "ejs");
 
 let items = ["code-development", "dsa"];
+let workItems = [];
 app.use(bodyParser.urlencoded({ extended:true}));
 
 app.use(express.static("public"));                 //serve static files like css and js
@@ -21,21 +23,40 @@ app.get("/", (req, res)=>{
     };
     let day = today.toLocaleDateString("en-US", options);
 
-    res.render("list", {kindOfDay: day, newListItems: items});
+    res.render("list", {listTitle: day, newListItems: items});
      
 });
 
 app.post("/", (req, res)=>{
     let item = req.body.newItem;
+    console.log(req.body);
+    if(req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }
+   
+    else{
+        items.push(item);
+        res.redirect("/");
+    }
 
-    items.push(item);
+   
 
-    res.redirect("/");
+    
     // console.log(item);
-}) 
+}) ;
+
+app.get("/work", (req, res)=>{
+    res.render("list", {listTitle: "Work List", newListItems: workItems})
+})
+app.post("/work", (res, render)=>{
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+})
 
 
 app.listen(3000, ()=>{
-    console.log("Server started at posrt 3000 ")
+    console.log("Server started at port 3000 ")
 })
 
