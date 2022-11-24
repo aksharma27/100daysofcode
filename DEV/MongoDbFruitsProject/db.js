1d1b5bcba263512efcd8216167a706447b0f05d1
abhishek-dev <enquireabhishek@gmail.com>
2022-11-24 23:14:41 +0530

    made fields in db using schema

--- DEV/MongoDbFruitsProject/db.js
+++ DEV/MongoDbFruitsProject/db.js
@@ -10,7 +10,15 @@ run()
 //create a new User object
 // const user = new User({name: "Abhishek", age: 20});
 //now save the user database, just a local copy in our program
-const user = await User.create({name: "Abhishek", age: 20})     //user created
+const user = await User.create({
+    name: "Abhishek",
+     age: 20, 
+     hobbies : ["weight lifting", "weight lifting"],
+     address: {
+        street: "hill road", 
+        city
+     }
+})     //user created
 user.name = "Sally";        //update/change some data in the db, but it is not saveed in the db, use .save() method
 await user.save();
 // await user.save();
