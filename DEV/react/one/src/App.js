d5131d25ae7efb32ad40fee7d3771b6d01ad2ed8
abhishek-dev <enquireabhishek@gmail.com>
2022-12-02 19:22:52 +0530

    inline styling using js objects

--- DEV/react/one/src/App.js
+++ DEV/react/one/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
+  
+  const time = new Date();
+  const ctime = time.getHours();
+  let greeting;
+
+  const constStyle = {
+    color : "red"
+  }
+
+  if(ctime < 12){
+    greeting = "Good morning";
+  }
+  else if(ctime > 12 && ctime < 6){
+    greeting = "Good Afternood";
+    constStyle.color = "green";
+  }
+  else{
+    greeting = "Good Evening";
+    constStyle.color = "blue";
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+    <h1 style={constStyle}>
+      {greeting}
+    </h1>
+   </>
   );
 }
 
