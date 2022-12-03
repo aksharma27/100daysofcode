c20bceeaf24037f09de907c19de52e7a6d1692e5
abhishek-dev <enquireabhishek@gmail.com>
2022-12-03 18:48:50 +0530

    added props to new function createEntry and called the props with id name emoji and meaning

--- DEV/react/mapping/src/components/App.jsx
+++ DEV/react/mapping/src/components/App.jsx
@@ -3,18 +3,23 @@ import Entry from './Entry'
 import '../App.css';
 import emojipedia from '../emojipedia';
 
+
+function createEntry(emojiTerm) {
+    return <Entry
+        key={emojiTerm.id}
+        emojie={emojiTerm.emoji}
+        name={emojiTerm.name}
+        description={emojiTerm.meaning}
+    />
+}
+
+
 const App = () => {
     return (
         <div>
             <h1>Emojipedia</h1>
             <dl className="dictionary">
-                <Entry
-                    emoji=
-                name=
-                description= 
-                 />
-                <Entry />
-                <Entry />
+                {emojipedia.map()}
             </dl>
         </div>
     )
