1cc361f4bc601751da3024baf4a09c858b8953bf
abhishek-dev <enquireabhishek@gmail.com>
2022-12-07 13:48:59 +0530

    redux - app.js reducer function made, counter.js

---
+++ DEV/redux/src/App.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+// import 
+import './App.css';
+
+
+const inititalState = {
+  count: 0
+}
+
+//create reducer function
+function countReducer(state = inititalState, )
+
+function App() {
+  return (
+    
+  );
+}
+
+export default App;
---
+++ DEV/redux/src/Counter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Counter = (props) => {
+    const count =  ; 
+  return (
+    <div>
+      Count is : {count};
+    </div>
+  )
+}
+
+export default Counter
